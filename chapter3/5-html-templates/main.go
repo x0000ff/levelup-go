df8a094df2f6508cb49074299df01a7f37bdadbe
Konstantin Portnov <konstantin.portnov@mercadolibre.cl>
2018-10-20 17:45:25 -0300

    Format with pipeline `printf`

--- chapter3/5-html-templates/main.go
+++ chapter3/5-html-templates/main.go
@@ -1,58 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"os"
 )
 
-// Article ...
-type Article struct {
-	Name       string
-	AuthorName string
-	Draft      bool
-}
-
-// Byline ...
-func (a Article) Byline() string {
-	return fmt.Sprintf("written by %s", a.AuthorName)
-}
-
 func main() {
 
-	tmpl, err := template.New("Foo").Parse(`
-    {{ define "ArticleResource" }}
-      <p>'{{ .Name }}' by {{ .AuthorName }}</p>
-    {{ end }}
+	tmpl, err := template.New("Foo").Parse("Price: ${{ . | printf \"%.2f\" }}\n")
 
-    {{ define "ArticleLoop" }}
-      {{ range . }}
-        {{ template "ArticleResource" . }}
-      {{ else }}
-        <p>No published articles yet</p>
-      {{ end }}
-    {{ end }}
-
-    {{ template "ArticleLoop" . }}
-    `)
-
-	articles := []Article{
-		Article{
-			Name:       "The Go html/template package",
-			AuthorName: "Mal Curtis",
-			Draft:      false,
-		},
-		Article{
-			Name:       "Code Complete",
-			AuthorName: "Steve McConnell",
-			Draft:      false,
-		},
-	}
 	if err != nil {
 		panic(err)
 	}
 
-	err = tmpl.Execute(os.Stdout, articles)
+	err = tmpl.Execute(os.Stdout, 12.3)
 
 	if err != nil {
 		panic(err)
