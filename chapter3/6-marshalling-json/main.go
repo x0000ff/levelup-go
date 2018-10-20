94aff86b0d665c280ace3dfaf4fd2b5b4f74d243
Konstantin Portnov <konstantin.portnov@mercadolibre.cl>
2018-10-20 18:10:42 -0300

    Custom JSON keys

--- chapter3/6-marshalling-json/main.go
+++ chapter3/6-marshalling-json/main.go
@@ -7,16 +7,22 @@ import (
 
 // Article ...
 type Article struct {
-	Name       string
-	AuthorName string
-	draft      bool
+	// Field appears in the JSON with the key "name"
+	Name string `json:"name"`
+
+	// Field appears in the JSON with the key "author_name"
+	// but doesn't appear at all if its value is empty
+	AuthorName string `json:"author_name,omitempty"`
+
+	// Field will not appear in the JSON representation.
+	ComissionPrice float64 `json:"-"`
 }
 
 func main() {
 	article := Article{
-		Name:       "JSON in Go",
-		AuthorName: "Mal Curtis",
-		draft:      true,
+		Name:           "JSON in Go",
+		AuthorName:     "Mal Curtis",
+		ComissionPrice: 23.1,
 	}
 
 	// data, err := json.Marshal(article)
