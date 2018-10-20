d11c25716df39a07a494e73a7e311da0b372f8f4
Konstantin Portnov <konstantin.portnov@mercadolibre.cl>
2018-10-20 17:52:05 -0300

    Format with custom pipelines

--- chapter3/5-html-templates/main.go
+++ chapter3/5-html-templates/main.go
@@ -5,15 +5,33 @@ import (
 	"os"
 )
 
+// Multiply ...
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+type Product struct {
+	Price    float64
+	Quantity float64
+}
+
 func main() {
 
-	tmpl, err := template.New("Foo").Parse("Price: ${{ . | printf \"%.2f\" }}\n")
+	tmpl := template.New("Foo")
+	tmpl.Funcs(template.FuncMap{
+		"multiply": Multiply,
+	})
+
+	tmpl, err := tmpl.Parse("Price: ${{ multiply .Price .Quantity | printf \"%.2f\" }}\n")
 
 	if err != nil {
 		panic(err)
 	}
 
-	err = tmpl.Execute(os.Stdout, 12.3)
+	err = tmpl.Execute(os.Stdout, Product{
+		Price:    12.3,
+		Quantity: 3,
+	})
 
 	if err != nil {
 		panic(err)
