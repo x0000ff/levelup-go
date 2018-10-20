e37c1fdc6a764ddaa696444f8455b43b44722100
Konstantin Portnov <konstantin.portnov@mercadolibre.cl>
2018-10-20 18:28:17 -0300

    Unmarshaling types

---
+++ chapter3/7-unmarshaling-json/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+// Config ...
+type Config struct {
+	Name     string `json:"SiteName"`
+	URL      string `json:"SiteUrl"`
+	Database struct {
+		Name     string
+		Host     string
+		Port     int
+		Username string
+		Password string
+	}
+}
+
+func main() {
+
+	conf := Config{}
+
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &conf)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Site: %s (%s)\n", conf.Name, conf.URL)
+
+	db := conf.Database
+
+	fmt.Printf("DB: mysql://%s:%s@%s:%d/%s\n",
+		db.Username,
+		db.Password,
+		db.Host,
+		db.Port,
+		db.Name,
+	)
+}
