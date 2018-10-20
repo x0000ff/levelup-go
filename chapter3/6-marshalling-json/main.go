f4c92c2c96036eb20d403345eb98952b1b61bb1c
Konstantin Portnov <konstantin.portnov@mercadolibre.cl>
2018-10-20 18:18:32 -0300

    Marshal nested types

--- chapter3/6-marshalling-json/main.go
+++ chapter3/6-marshalling-json/main.go
@@ -7,26 +7,31 @@ import (
 
 // Article ...
 type Article struct {
-	// Field appears in the JSON with the key "name"
 	Name string `json:"name"`
+}
 
-	// Field appears in the JSON with the key "author_name"
-	// but doesn't appear at all if its value is empty
-	AuthorName string `json:"author_name,omitempty"`
-
-	// Field will not appear in the JSON representation.
-	ComissionPrice float64 `json:"-"`
+// ArticleCollection ...
+type ArticleCollection struct {
+	Articles []Article `json:"articles"`
+	Total    int       `json:"total"`
 }
 
 func main() {
-	article := Article{
-		Name:           "JSON in Go",
-		AuthorName:     "Mal Curtis",
-		ComissionPrice: 23.1,
+	article1 := Article{
+		Name: "JSON in Go",
 	}
 
-	// data, err := json.Marshal(article)
-	data, err := json.MarshalIndent(article, "", "   ")
+	article2 := Article{
+		Name: "Marshaling is easy",
+	}
+
+	articles := []Article{article1, article2}
+	collection := ArticleCollection{
+		Articles: articles,
+		Total:    len(articles),
+	}
+
+	data, err := json.MarshalIndent(collection, "", "   ")
 	if err != nil {
 		fmt.Println("Couldn't marshal article:", err)
 	} else {
